4f2c8722b9c98e686313b2179e6c663aed86b016
Babatunde AbdulHameed <humiditii45@gmail.com>
2024-05-29 17:18:45 +0100

    fix: make var secrets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Room } from './room/dbLayer/room.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+
+console.log('debug', process.env.DB_URL)
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      // host: process.env.DB_HOST,
-      // port: <number>(process.env.DB_PORT as any),
-      // username: process.env.DB_USER,
-      // password: process.env.PASSWORD,
-      database: process.env.DB_INUSE,
-      entities: [Room],
-      url:process.env.DB_URL,
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DB_URL'),
+        entities: [
+          Room
+        ],
+        ssl: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService]
+    })
   ],
 })
 export class AppModule { }
+
+
